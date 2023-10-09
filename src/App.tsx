63177edaf33ef14b09cd97459e5e4afb3bf25ae3
LJoel777 <szaknoel@gmail.com>
2023-10-09 12:29:10 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex h-screen w-screen justify-center items-center bg-red-100">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
